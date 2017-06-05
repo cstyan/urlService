7ef758469104c7942e118a463e5df62f6a0f0708
Callum Styan <callumstyan@gmail.com>
2017-06-04 23:21:09 -0700

    add test helper script, run it in build script

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 
 VERSION="0.1"
 
+
 go get github.com/gorilla/mux
 # since we're using scratch image we need to staticly compile in C libraries
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo
+
+./test.sh
+
 docker build -t url-service:latest -t url-service:"$VERSION" .
---
+++ test.sh
@@ -0,0 +1,2 @@
+go test ./dataStore -v
+go test -v
