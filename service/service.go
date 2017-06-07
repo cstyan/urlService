54e970940238a1e34b180b501831331cda8aaaf7
Callum Styan <callumstyan@gmail.com>
2017-06-06 19:29:48 -0700

    this query function in service.go is not used

--- service/service.go
+++ service/service.go
@@ -12,10 +12,6 @@ type Service struct {
 	Router    *mux.Router
 }
 
-func (s *Service) queryDataStore(url string) bool {
-	return s.DataStore.Query(url)
-}
-
 func (s *Service) InitService() {
 	// is there an easier way to replace the data store type
 	// TODO: error check here for other data store types
